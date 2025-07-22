c5f547065a1447b8add4c0b76eed073ec11d79a5
ivanshifman <ivanshifman1300@gmail.com>
2025-07-22 00:10:41 -0300

    fix index server

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
+app.set('trust proxy', 1);
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(loggerMiddleware);
 app.use(generalLimiter);
